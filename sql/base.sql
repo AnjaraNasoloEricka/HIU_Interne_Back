4c2c94555102bcad27ae0a72f83e28de72617cea
ShinyIratra <Iratra53@gmail.com>
2025-03-29 12:07:39 +0300

    chore : add sql

--- sql/base.sql
+++ sql/base.sql
@@ -1,54 +1,18 @@
-create database db3;
-use db3;
+create database HIU;
+use HIU;
 
-create table td3_User(
-    id integer auto_increment primary key,
-    username varchar(20),
-    pwd varchar(50)
+create table User
+(
+    id_user int auto_increment primary key,
+    name varchar(100),
+    password varchar(100)
 );
 
-create table td3_Produit(
-    id integer auto_increment primary key,
-    libelle varchar(50),
-    prix integer
+create table Article
+(
+    id_article int auto_increment primary key,
+    title varchar(255),
+    description text,
+    date_create Date,
+    date_modification Date
 );
-
-create table td3_Caisse(
-    id integer auto_increment primary key,
-    num_caisse integer
-);
-
-create table td3_Achat(
-    id integer auto_increment primary key,
-    date_achat datetime
-);
-
-create table td3_rel_Achat_Produit(
-    id_produit integer,
-    id_achat integer,
-    quantite integer,
-    foreign key (id_produit) references td3_Produit(id),
-    foreign key (id_achat) references td3_Achat(id)
-);
-
-create table td3_Historique_Achat(
-    id integer auto_increment primary key,
-    id_caisse integer,
-    id_produit integer,
-    quantite integer,
-    montant integer,
-    date_achat datetime,
-    foreign key (id_caisse) references td3_Caisse(id),
-    foreign key (id_produit) references td3_Produit(id)
-);
-
-create table td3_Fluctuation_Qte_Produit(
-    id integer auto_increment primary key,
-    id_produit integer,
-    fluctuation integer,
-    foreign key (id_produit) references td3_Produit(id)
-
-);
-
-insert into td3_Caisse(num_caisse) 
-values (1),(2),(5);
